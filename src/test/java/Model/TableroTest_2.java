ed7e4387bdf251ef9d48c512f8dabb89e5c12a9b
Rudyx28 <ruben.campuzano@e-campus.uab.cat>
2020-11-29 22:27:38 +0100

    ESCENARIO 2 + ataque 1 y 3 v1

--- src/test/java/Model/TableroTest_2.java
+++ src/test/java/Model/TableroTest_2.java
@@ -34,9 +34,58 @@ public class TableroTest_2 {
 		assertFalse(tablero.getCasilla(10, 10).getMarcada());
 	}
 	
+	/* ESCENARIO 2 - ABRIR UNA CASILLA */
+	@Test
+	public void escenario2() {
+		JuegoMock juegoMock = new JuegoMock();
+		int [][] posiciones = juegoMock.posicionesAleatoriasTablero();
+		Tablero tablero = new Tablero(9,9);
+		tablero.initBoard();
+		tablero.colocarMinas(posiciones);
+		
+		
+		//Caso 1 - Abrir casilla cerrada y que se encuentre dentro del rango del tablero.
+		tablero.abrirCasilla(1, 1);
+		assertTrue(tablero.getCasilla(1, 1).getAbierta());
+		
+		//Caso 2 - Abrir casilla ya abierta.
+		tablero.abrirCasilla(2, 2);
+		tablero.abrirCasilla(2, 2);
+		assertTrue(tablero.getCasilla(2, 2).getAbierta());
+		
+		//Caso 3 - Abrir casilla ya marcada.
+		tablero.marcarCasilla(3, 3);
+		tablero.abrirCasilla(3, 3);
+		assertFalse(tablero.getCasilla(3, 3).getAbierta());
+		
+		//Caso 4 - Abrir casilla fuera del rango del tablero.
+		tablero.abrirCasilla(10, 10);
+		assertFalse(tablero.getCasilla(10, 10).getAbierta());
+	}	
 	
+	@Test
+	public void Ataque1() {
+		JuegoMock juegoMock = new JuegoMock();
+		int [][] posiciones = juegoMock.posicionesAleatoriasTablero();
+		Tablero tablero = new Tablero(9,9);
+		tablero.initBoard();
+		tablero.colocarMinas(posiciones);
+		
+		tablero.abrirCasilla(2, B);
+			
+	}
 	
-	
+	@Test
+	public void Ataque3() {
+		JuegoMock juegoMock = new JuegoMock();
+		int [][] posiciones = juegoMock.posicionesAleatoriasTablero();
+		Tablero tablero = new Tablero(9,9);
+		tablero.initBoard();
+		tablero.colocarMinas(posiciones);
+		
+		tablero.abrirCasilla(null, null);
+			
+	}
 	
 	
 	
